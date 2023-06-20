872d4cf54c2e53e767195ea0a77b53e675dfd2b8
danhursty <daniel.d.hurst@gmail.com>
2023-06-20 09:36:06 +0100

    Cleaned up base.js

--- src/base.js
+++ src/base.js
@@ -8,13 +8,6 @@ const firebaseApp = firebase.initializeApp({
         databaseURL: "https://catch-of-the-day-d6437-default-rtdb.europe-west1.firebasedatabase.app"
       });
 
-
-// const firebaseApp = firebase.initializeApp({
-//         apiKey: "AIzaSyCxdhFcN5p9shE-LjL7wbC6lo9JtY_jboM",
-//         authDomain: "catch-of-the-day-d6437.firebaseapp.com",
-//         databaseURL: "https://catch-of-the-day-d6437-default-rtdb.europe-west1.firebasedatabase.app"
-//       });
-
 const base = Rebase.createClass(firebaseApp.database());
 
 // This is a named export
